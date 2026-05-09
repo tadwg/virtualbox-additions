f563545f6142098cc2a941ad454dfc8ce2f3f28b
paijp <root@php.pa-i.org>
2026-05-09 21:42:28 +0900

    Add Claude Sonnet 4.6 attribution to source header

--- src/vboxmousegpl.c
+++ src/vboxmousegpl.c
@@ -15,7 +15,10 @@
  * ソースコード:
  *   https://github.com/tadwg/virtualbox-additions
  *
- * Copyright (C) 2024
+ * Copyright (C) 2026
+ *
+ * このソースコードは Claude Sonnet 4.6 の支援を受けて書かれました。
+ * Written with the assistance of Claude Sonnet 4.6 (Anthropic).
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
